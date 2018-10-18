3497400024fccb13ea65435e38249c9b5607be6c
Artem Semavin <ooodin.box@gmail.com>
2018-10-19 01:09:01 +0300

    Initial Commit

---
+++ DTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DTrip
+//
+//  Created by Artem Semavin on 19/10/2018.
+//  Copyright © 2018 Semavin Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
