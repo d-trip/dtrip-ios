0c0419c14c1044dc7eef31629c00dfc9d8b89b8b
Artem Semavin <ooodin.box@gmail.com>
2018-11-25 23:17:06 +0300

    deleted PostItem

--- DTrip/Flows/Post/PostViewModel.swift
+++ DTrip/Flows/Post/PostViewModel.swift
@@ -3,7 +3,7 @@ import RxSwift
 import RxCocoa
 
 protocol PostViewModel {
-    var post: Driver<PostItem> { get }
+//    var post: Driver<PostItem> { get }
     var setPostIdentifier: AnyObserver<PostIdentifier> { get }
     var setPostModel: AnyObserver<PostModel> { get }
     var disposeBag: DisposeBag { get }
@@ -15,10 +15,10 @@ final class PostViewModelImp: PostViewModel {
 
     let disposeBag = DisposeBag()
 
-    var post: Driver<PostItem> {
-        let error: PostItem = .errorItem(title: "Error")
-        return postSubject.asDriver(onErrorJustReturn: error)
-    }
+//    var post: Driver<PostItem> {
+//        let error: PostItem = .errorItem(title: "Error")
+//        return postSubject.asDriver(onErrorJustReturn: error)
+//    }
 
     var setPostIdentifier: AnyObserver<PostIdentifier> {
         return postIdentifierSubject.asObserver()
@@ -30,7 +30,7 @@ final class PostViewModelImp: PostViewModel {
 
     // MARK: - Private
 
-    private let postSubject = ReplaySubject<PostItem>.create(bufferSize: 1)
+//    private let postSubject = ReplaySubject<PostItem>.create(bufferSize: 1)
     private let postIdentifierSubject = ReplaySubject<PostIdentifier>.create(bufferSize: 1)
     private let postModelSubject = ReplaySubject<PostModel>.create(bufferSize: 1)
 
@@ -46,14 +46,14 @@ final class PostViewModelImp: PostViewModel {
             .bind(to: postModelSubject.asObserver())
             .disposed(by: disposeBag)
 
-        postIdentifierSubject
-            .map { _ in PostItem.loadingItem(title: "Loading...", animate: true) }
-            .bind(to: postSubject.asObserver())
-            .disposed(by: disposeBag)
-
-        postModelSubject
-            .map { PostItem.postItem(post: $0) }
-            .bind(to: postSubject.asObserver())
-            .disposed(by: disposeBag)
+//        postIdentifierSubject
+//            .map { _ in PostItem.loadingItem(title: "Loading...", animate: true) }
+//            .bind(to: postSubject.asObserver())
+//            .disposed(by: disposeBag)
+//
+//        postModelSubject
+//            .map { PostItem.postItem(post: $0) }
+//            .bind(to: postSubject.asObserver())
+//            .disposed(by: disposeBag)
     }
 }
